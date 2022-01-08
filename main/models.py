cd096f05483b9a48698b1cedd2318a7123c701c6
nucluster <nucluster@gmail.com>
2022-01-08 21:02:10 +0500

    add model State

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField('Название штата', max_length=50, unique=True)
+    zip_code = models.CharField('Почтовый код', max_length=2, unique=True)
+    capital = models.CharField('Столица штата', max_length=50)
+    largest_city = models.CharField('Крупнейший город', max_length=50,
+                                    blank=True)
+    ratification = models.DateField('Дата присоединения')
+    population = models.IntegerField('Население')
+    total_area = models.DecimalField('Площадь, кв.км', max_digits=10,
+                                    decimal_places=2)
+    land_area = models.DecimalField('Площадь суши, кв.км', max_digits=10,
+                                    decimal_places=2)
+    water_area = models.DecimalField('Площадь воды, кв.км', max_digits=10,
+                                    decimal_places=2)
+
+    def __str__(self):
+        return self.name
