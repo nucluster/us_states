ff7781464165115867284bb9422fbdb7d4a63e64
nucluster <nucluster@gmail.com>
2022-01-08 19:54:07 +0500

    start prj and main

---
+++ us_states/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for us_states project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'us_states.settings')
+
+application = get_asgi_application()
