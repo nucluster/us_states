a705699417f405be91b2f5db97d1acbf6aed46f8
nucluster <nucluster@gmail.com>
2022-01-11 20:48:11 +0500

    cosmetics

--- main/views.py
+++ main/views.py
@@ -3,14 +3,8 @@ import django_tables2 as tables
 from .models import State, StateTable
 
 
-# def hello(request):
-#     return render(request, 'main/hello.html', {})
-
-
 class TableView(tables.SingleTableView):
     table_class = StateTable
     queryset = State.objects.all()
     template_name = "main/hello.html"
     table_pagination = False
-
-# tables.SingleTableView.table_pagination = False
