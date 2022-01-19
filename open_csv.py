1758e975bac93043610aef6d36ceaf5de7dae78f
nucluster <nucluster@gmail.com>
2022-01-19 23:45:52 +0500

    create dirs in media/

--- open_csv.py
+++ open_csv.py
@@ -1,8 +1,22 @@
 import csv
+import os
 
 new_lst = []
 
 with open('us_states.csv') as f:
     reader = csv.DictReader(f)
     for row in reader:
-        print(row)
+        new_lst.append(row['ZIP code'])
+
+print(new_lst)
+
+BASIC_DIR = 'media/'
+
+for code in new_lst:
+    dirName = os.path.join(BASIC_DIR, code)
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
+        print("Directory " , dirName ,  " Created ")
+    else:
+        print("Directory " , dirName ,  " already exists")
+
