7222bed8b76fd602049bf0534afb2b76c8c84655
nucluster <nucluster@gmail.com>
2022-01-20 17:36:09 +0500

    pep8

--- main/tables.py
+++ main/tables.py
@@ -6,21 +6,24 @@ from .models import State
 class ImageColumnFlag(tables.Column):
     def render(self, value):
         return format_html(
-            '<a href="/media/{0}"><img src="/media/{0}" width="100" height="66" />', value)
+            '<a href="/media/{0}"><img src="/media/{0}" width="100" '
+            'height="66" />', value)
+
 
 class ImageColumnSeal(tables.Column):
     def render(self, value):
         return format_html(
-            '<a href="/media/{0}"><img src="/media/{0}" width="50" height="50" />', value)
+            '<a href="/media/{0}"><img src="/media/{0}" width="50" '
+            'height="50" />', value)
+
 
 class StateTable(tables.Table):
     flag_image = ImageColumnFlag()
     seal_image = ImageColumnSeal()
+
     class Meta:
         model = State
-        attrs = {"class":"table table-striped table-bordered"}
+        attrs = {"class": "table table-striped table-bordered"}
         fields = ('id', 'name', 'zip_code', 'capital', 'largest_city',
-                    'ratification', 'population', 'total_area', 'land_area',
-                    'water_area', 'flag_image', 'seal_image')
-
-
+                  'ratification', 'population', 'total_area', 'land_area',
+                  'water_area', 'flag_image', 'seal_image')
