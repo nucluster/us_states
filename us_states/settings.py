161aeda659eea02877a65e1ba409a24e7ad34c39
vladimir-asus <vladimir@vladimir-e200ha>
2022-03-08 01:04:59 +0500

    Update settings STATIC_ROOT

--- us_states/settings.py
+++ us_states/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
-# STATIC_ROOT = BASE_DIR.joinpath('static/')
+STATIC_ROOT = BASE_DIR.joinpath('static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media/')
